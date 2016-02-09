3dc6144b4af9691a904bfe3a307e50df336fdacc
alemosie <alexandra.siega@gmail.com>
2016-02-09 09:43:53 -0500

    loop do and until loop versions

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,30 @@
 
 def levitation_quiz
-	#your code here
+	loop do
+        puts "What is the spell that enacts levitation?"
+        answer = gets.chomp
+        if answer == "Wingardium Leviosa"
+            puts "You passed the quiz!"
+            break
+        end
+    end
 end
 
 
+# Wanted to try an until loop
+
+=begin
+ 
+def qAndA
+    puts "What is the spell that enacts levitation?"
+    $answer = gets.chomp
+end
+
+def levitation_quiz
+    until $answer == "Wingardium Leviosa"
+        qAndA
+    end
+    puts "You passed the quiz!"
+end
+ 
+=end
